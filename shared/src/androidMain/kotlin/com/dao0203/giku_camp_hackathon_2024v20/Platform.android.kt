3c07b24763c799edf073c83369438f85cbc2b347
dao0203 <yuyudevelop0203@gmail.com>
2025-01-11 17:47:33 +0900

    delete unused class named Platform

--- shared/src/androidMain/kotlin/com/dao0203/giku_camp_hackathon_2024v20/Platform.android.kt
+++
@@ -1,5 +0,0 @@
-package com.dao0203.giku_camp_hackathon_2024v20
-
-actual class Platform actual constructor() {
-    actual val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
--- shared/src/commonMain/kotlin/com/dao0203/giku_camp_hackathon_2024v20/SharedModule.kt
+++ shared/src/commonMain/kotlin/com/dao0203/giku_camp_hackathon_2024v20/SharedModule.kt
@@ -1,3 +1,5 @@
 package com.dao0203.giku_camp_hackathon_2024v20
 
-fun sharedModule() = listOf(platformModule)
+import org.koin.core.module.Module
+
+fun sharedModule() = listOf<Module>()
