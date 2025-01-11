b37fb0666b23eb75b6bd858f89b1cf6998679a8f
dao0203 <yuyudevelop0203@gmail.com>
2025-01-11 15:36:19 +0900

    update java version

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         minSdk = 26
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
