aa9fc65292f6f19e6e5a45fe07641f98fd6acffc
dao0203 <yuyudevelop0203@gmail.com>
2025-01-12 00:55:28 +0900

    add viewmodel to sharedmodule

--- shared/src/commonMain/kotlin/com/dao0203/giku_camp_hackathon_2024v20/SharedModule.kt
+++ shared/src/commonMain/kotlin/com/dao0203/giku_camp_hackathon_2024v20/SharedModule.kt
@@ -1,5 +1,9 @@
 package com.dao0203.giku_camp_hackathon_2024v20
 
-import org.koin.core.module.Module
+import com.dao0203.giku_camp_hackathon_2024v20.feature.training.DefinitionMenuViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
 
-fun sharedModule() = listOf<Module>()
+fun sharedModule() = module {
+    viewModel { DefinitionMenuViewModel() }
+}
