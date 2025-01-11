3ca3186377817efb1ef8fd57b4c813f16365a7fa
dao0203 <yuyudevelop0203@gmail.com>
2025-01-11 17:42:55 +0900

    add Main Navigation and NavHost

---
+++ androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/MainNavigation.kt
@@ -0,0 +1,23 @@
+package com.dao0203.giku_camp_hackathon_2024v20.android
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.dao0203.giku_camp_hackathon_2024v20.android.feature.training.navigation.TrainingRoute
+import com.dao0203.giku_camp_hackathon_2024v20.android.feature.training.navigation.trainingNavigation
+import com.dao0203.giku_camp_hackathon_2024v20.android.ui.MainAppState
+
+@Composable
+fun MainNavHost(
+    mainAppState: MainAppState,
+    modifier: Modifier = Modifier,
+) {
+    val navController = mainAppState.navController
+    NavHost(
+        navController = navController,
+        startDestination = TrainingRoute.Base,
+        modifier = modifier,
+    ) {
+        trainingNavigation()
+    }
+}
