da68190ca530219fa681d7824f93d111aad4474b
dao0203 <yuyudevelop0203@gmail.com>
2025-01-12 00:55:19 +0900

    rename

--- androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/feature/training/navigation/TrainingNavigation.kt
+++ androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/feature/training/navigation/TrainingNavigation.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.dao0203.giku_camp_hackathon_2024v20.android.feature.training.DefinitionMenuScreen
 import kotlinx.serialization.Serializable
 
 sealed interface TrainingRoute {
