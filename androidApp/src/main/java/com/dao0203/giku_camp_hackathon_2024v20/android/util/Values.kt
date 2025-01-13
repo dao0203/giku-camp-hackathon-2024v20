6ceae090cc7455001bf1bc7e8a7d28855afb147f
dao0203 <yuyudevelop0203@gmail.com>
2025-01-13 12:49:42 +0900

    add typealias

--- androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/util/Values.kt
+++ androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/util/Values.kt
@@ -1,3 +1,4 @@
 package com.dao0203.giku_camp_hackathon_2024v20.android.util
 
 typealias StringRes = com.dao0203.giku_camp_hackathon_2024v20.android.R.string
+typealias DrawableRes = com.dao0203.giku_camp_hackathon_2024v20.android.R.drawable
