e61c4c453f3770e342c7aa49845d7b2e242282b8
dao0203 <yuyudevelop0203@gmail.com>
2025-01-13 12:10:13 +0900

    add utils

---
+++ androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/util/ComposeUtil.kt
@@ -0,0 +1,6 @@
+package com.dao0203.giku_camp_hackathon_2024v20.android.util
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(locale = "ja", showBackground = true)
+annotation class MainPreview
---
+++ androidApp/src/main/java/com/dao0203/giku_camp_hackathon_2024v20/android/util/Values.kt
@@ -0,0 +1,3 @@
+package com.dao0203.giku_camp_hackathon_2024v20.android.util
+
+typealias StringRes = com.dao0203.giku_camp_hackathon_2024v20.android.R.string
