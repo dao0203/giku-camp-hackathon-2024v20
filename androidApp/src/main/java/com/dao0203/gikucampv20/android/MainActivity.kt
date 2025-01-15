19c97ea29fa3a5fe9cca32874a063014fffd9e1b
dao0203 <yuyudevelop0203@gmail.com>
2025-01-15 22:21:23 +0900

    remove unused composable

--- androidApp/src/main/java/com/dao0203/gikucampv20/android/MainActivity.kt
+++ androidApp/src/main/java/com/dao0203/gikucampv20/android/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.dao0203.gikucampv20.android.ui.rememberMainAppState
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +16,7 @@ class MainActivity : ComponentActivity() {
             MainTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     MainNavHost(
                         mainAppState = rememberMainAppState(),
@@ -27,18 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MainTheme {
-        GreetingView("Hello, Android!")
     }
 }
