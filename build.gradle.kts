89bc42f79f3f7dbca0efcf11a23efd5db81b7870
dao0203 <yuyudevelop0203@gmail.com>
2025-01-15 22:07:42 +0900

    add klint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    // trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
@@ -7,19 +7,5 @@ plugins {
     alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.serialization).apply(false)
     alias(libs.plugins.skie).apply(false)
-    alias(libs.plugins.spotless)
-}
-
-
-subprojects {
-    apply(plugin = "com.diffplug.spotless")
-    spotless {
-        kotlin {
-            ktlint("1.0.0")
-            target("**/*.kt")
-            target("**/*.kts")
-            targetExclude("**/build/**/*.kt")
-            targetExclude("**/build/**/*.kts")
-        }
-    }
+    alias(libs.plugins.ktlint).apply(false)
 }
