624ad630cc1cb4fd5756b14d03556396f8383d32
dao0203 <yuyudevelop0203@gmail.com>
2025-01-18 04:21:39 +0900

    add new screen folder

---
+++ iosApp/iosApp/Feature/Training/MenuDefinitionScreen.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import GikuCampKit
+
+struct MenuDefinitionScreen: View {
+	let storeOwner = CustomViewModelStoreOwner <MenuDefinitionViewModel> ()
+
+	var body: some View {
+		Observing(storeOwner.instance.uiState) { uiState in
+			VStack {
+//				List {
+//                    Section(header: Text("Training Menu")) {
+//                        TrainingMenuInputs(uiState: uiState)
+//                    }
+//                    Section(header: Text("Training Types")) {
+//                        ForEach(uiState.muscleGroupsUiModel.trainingTypesByGroup.keys.sorted(by: { $0.name < $1.name })) { muscleGroup in
+//                            Text(muscleGroup.name)
+//                            ForEach(uiState.muscleGroupsUiModel.trainingTypesByGroup[muscleGroup] ?? []) { trainingType in
+//                                TrainingTypeItem(trainingType: trainingType) {
+//                                    storeOwner.instance.changeTrainingType($0)
+//                                }
+//                            }
+//                        }
+//                    }
+//                }.listStyle(InsetGroupedListStyle)
+			}
+		}
+	}
+}
