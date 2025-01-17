e4f3796f2ab79f6c8166fe282f1d34db9b600851
dao0203 <yuyudevelop0203@gmail.com>
2025-01-17 17:52:16 +0900

    implement accelerometer ancestor

--- androidApp/src/main/java/com/dao0203/gikucampv20/android/feature/training/TrainingWithCameraScreen.kt
+++ androidApp/src/main/java/com/dao0203/gikucampv20/android/feature/training/TrainingWithCameraScreen.kt
@@ -1,5 +1,7 @@
 package com.dao0203.gikucampv20.android.feature.training
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.camera.core.ImageProxy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,15 +17,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.getSystemService
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dao0203.gikucampv20.android.BuildConfig
 import com.dao0203.gikucampv20.android.R
@@ -39,6 +45,22 @@ fun TrainingWithCameraScreen(
 ) {
     val viewModel = koinViewModel<TrainingWithCameraViewModel>()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    val sensorManager = remember { context.getSystemService(SensorManager::class.java) }
+
+    DisposableEffect(Unit) {
+        val accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        sensorManager?.registerListener(
+            viewModel,
+            accelerometer,
+            SensorManager.SENSOR_DELAY_NORMAL,
+        )
+        onDispose {
+            sensorManager?.unregisterListener(viewModel, accelerometer)
+        }
+    }
+
     LaunchedEffect(Unit) {
         viewModel.initialize()
         viewModel.effect.collect {
