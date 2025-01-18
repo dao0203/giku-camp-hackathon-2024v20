3e358e7efdd607dbfddf3eae6470b08170bed01c
dao0203 <yuyudevelop0203@gmail.com>
2025-01-18 17:23:06 +0900

    refactor

--- androidApp/src/main/java/com/dao0203/gikucampv20/android/util/ComposeUtil.kt
+++ androidApp/src/main/java/com/dao0203/gikucampv20/android/util/ComposeUtil.kt
@@ -5,6 +5,6 @@ package com.dao0203.gikucampv20.android.util
 import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(locale = "ja", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview(locale = "ja", showBackground = true)
+@Preview(locale = "ja", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 annotation class MainPreviews
